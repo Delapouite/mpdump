0166b4ee7a1c8f5e242cbab148724da3986d64a1
Delapouite <delapouite@gmail.com>
2023-01-01 14:49:30 +0100

    feat(parse): output warnings for exotic tags

--- dump.js
+++ dump.js
@@ -8,7 +8,7 @@ const client = net.connect(6600, () => client.write("listallinfo\n"))
 client.on("data", (data) => {
   const str = data.toString()
   console.log(str)
-  if (str.endsWith('OK\n')) {
+  if (str.endsWith("OK\n")) {
     client.destroy()
   }
 })
--- parse.js
+++ parse.js
@@ -1,53 +1,69 @@
-var fs = require("fs")
-var camelCase = require("camelcase")
+const fs = require("fs")
+const camelCase = require("camelcase")
+
+const tags = [
+  "file",
+  "lastModified",
+  "format",
+  "time",
+  "duration",
+  "artist",
+  "title",
+  "album",
+  "track",
+  "date",
+]
 
 fs.readFile("dump.raw", "utf8", (err, content) => {
+  if (err) throw err
+
+  const parsed = parse(content)
+
+  fs.writeFile("dump.json", JSON.stringify(parsed, null, 2), (err) => {
     if (err) throw err
 
-    var parsed = parse(content)
-
-    fs.writeFile("dump.json", JSON.stringify(parsed, null, 2), (err) => {
-        if (err) throw err
-
-        console.log(`dump.json created with ${parsed.songs.length} songs`)
-    })
+    console.log(`dump.json created with ${parsed.songs.length} songs`)
+  })
 })
 
 function parse (content) {
-    var song = null
-    var last = null
+  let song = null
+  let last = null
 
-    var songs = getLines(content).reduce((acc, line) => {
-        const [key, value] = getEntry(line)
+  const songs = getLines(content).reduce((acc, line) => {
+    const [key, value] = getEntry(line)
+    if (key === "directory") {
+      console.log(value)
+      last = key
+    } else {
+      if (!tags.includes(key))
+        console.warn("tag", key, value)
+    }
+    // song delimiter
+    if (key === "file") {
+      last = key
+      if (song) acc.push(song)
+        song = { file: value }
+    } else if (last === "file") {
+      song[key] = value
+    }
+    return acc
+  }, [])
 
-        if (key === "directory") {
-            last = key
-        }
-        // song delimiter
-        if (key === "file") {
-            last = key
-            if (song) acc.push(song)
-            song = { file: value }
-        } else if (last === "file") {
-            song[key] = value
-        }
-        return acc
-    }, [])
-
-    return { songs }
+  return { songs }
 }
 
 function getLines (content) {
-    var lines = content.split("\n")
-    // remove extraneous MPD fences
-    lines.shift()
-    lines.pop()
-    lines.pop()
+  const lines = content.split("\n")
+  // remove extraneous MPD fences
+  lines.shift()
+  lines.pop()
+  lines.pop()
 
-    return lines
+  return lines
 }
 
 function getEntry (line) {
-    var s = line.split(":")
-    return [camelCase(s[0]), s[1].trim()]
+  const [k, v] = line.split(":")
+  return [camelCase(k), v.trim()]
 }
